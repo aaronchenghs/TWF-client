b857a8027979d57a0e62a3a9f5dac9c025d0f7b7
Aaron Cheng <acheng8@lsu.edu>
2026-01-19 20:34:53 -0600

    rename initSocketErrorToast

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./store/store";
-import { initSocketErrorToasts } from "./lib/errorHandling";
+import { initSocketErrorToasts as initSocketErrorHandler } from "./lib/errorHandling";
 
 // Initialized in main to avoid StrictMode and Hot-Reload double-invoking issues
-initSocketErrorToasts();
+initSocketErrorHandler();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
